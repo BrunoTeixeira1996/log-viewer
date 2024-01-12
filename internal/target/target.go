7c8343cc23e8e668cc77d84eb593c47f0e503685
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2024-01-12 16:28:18 +0000

    target: added nmap for gokrazy
    Since gokrazy has a Busybox shell it does not come with nmap installed
    so I decided to use a nmap implementation in Go

--- internal/target/target.go
+++ internal/target/target.go
@@ -1,8 +1,14 @@
 package target
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"os/exec"
 	"strings"
+	"time"
+
+	"github.com/Ullaakut/nmap/v3"
 )
 
 type Target struct {
@@ -11,15 +17,63 @@ type Target struct {
 	Status string
 }
 
-func (t *Target) IsListening(ip string) {
-	out, err := exec.Command("nmap", "-p", "9292", ip).Output()
+func nmapForGokrazy(ip string) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	scanner, err := nmap.NewScanner(ctx, nmap.WithTargets(ip), nmap.WithPorts("9292"))
+
 	if err != nil {
-		t.Status = "unavailable"
+		return false, fmt.Errorf("[ERROR] unable to create nmap scanner: %v", err)
 	}
 
-	if strings.Contains(string(out), "open") {
-		t.Status = "available"
-	} else {
-		t.Status = "unavailable"
+	result, _, err := scanner.Run()
+	if err != nil {
+		return false, fmt.Errorf("[ERROR] unable to run nmap scan: %v", err)
 	}
+
+	// Check if Host is up
+	if result.Stats.Hosts.Up == 0 {
+		return false, nil
+	}
+
+	// Check if port 9292 is open
+	for _, port := range result.Hosts[0].Ports {
+		if port.ID == 9292 {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func (t *Target) IsListening(ip string, isGokrazy bool) {
+	switch isGokrazy {
+	case false:
+		out, err := exec.Command("nmap", "-p", "9292", ip).Output()
+		if err != nil {
+			t.Status = "unavailable"
+		}
+
+		if strings.Contains(string(out), "open") {
+			t.Status = "available"
+		} else {
+			t.Status = "unavailable"
+		}
+
+	case true:
+		isPortOpen, err := nmapForGokrazy(ip)
+		if err != nil {
+			log.Println("[ERROR] while doing nmapForGokrazy:", err)
+			break
+		}
+
+		if isPortOpen {
+			t.Status = "available"
+		} else {
+			t.Status = "unavailable"
+		}
+
+	}
+
 }
